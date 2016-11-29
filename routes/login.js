9f22d78fb8dba789680d9797c6f7d287c339eb9a
zhengyaing <wang_zhengya@163.com>
2016-11-29 11:33:19 +0800

    login pages

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET login page. */
 router.get('/', function(req, res, next) {
   res.render('login', { title: '山东航空北京维修基地OA' });
+});
+
+/* GET home page. */
+router.get('/login', function(req, res, next) {
+  res.render('index', { title: '登陆成功过' });
 });
 
 module.exports = router;
